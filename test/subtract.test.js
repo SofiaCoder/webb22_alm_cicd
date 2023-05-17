ab35258127dfc9de14229b211d64e3674b950eb0
Sofia <sofia.laas@gmail.com>
2023-05-17 22:16:48 +0200

    Update subtract.test.js

--- test/subtract.test.js
+++ test/subtract.test.js
@@ -1,11 +1,1 @@
-const request = require('supertest');
-const { expect } = require('chai');
-const app = require('../app');
 
-describe('GET /', () => {
-  it('Testing index endpoint', async () => {
-    const response = await request(app).get('/sub/6&4');
-
-    expect(response.text).to.equal('Result: 2');
-  });
-});
