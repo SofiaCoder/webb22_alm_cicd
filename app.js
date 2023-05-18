e72f6d5be7bc3871281187d8516d13d5129d3c0f
sofiaCoder <sofia.laas@gmail.com>
2023-05-18 22:57:57 +0200

    Deleted empty files

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const adduction = require('./routes/calc');
-const subtract = require('./routes/subtract');
 
 const app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/add', adduction);
-app.use('/sub', subtract);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
